9f4a7c54f690d30b670381ba68b44e94ae8df28c
humblex <praneeykauri@gmail.com>
2022-03-09 11:19:31 +0545

    circles added

---
+++ circle.cpp
@@ -0,0 +1,50 @@
+#include "circle.h"
+#include <QGraphicsScene>
+#include <QDebug>
+
+Circle::Circle()
+{
+
+}
+
+Circle::Circle(int x)
+{
+    setRect(x,0,50,50);
+    if(x<300){
+    setBrush(QBrush(QColor(5, 235, 55),Qt::SolidPattern));//green
+    setPen(QPen(QColor(15, 150, 22)));
+    }
+    else{
+    setBrush(QBrush(QColor(235, 235, 5),Qt::SolidPattern));//yellow
+    setPen(QPen(QColor(235, 181, 5)));
+    }
+
+    //connect
+    timer = new QTimer;
+    connect(timer,SIGNAL(timeout()),this,SLOT(move())) ;
+    timer->start(100);
+}
+Circle::~Circle(){
+    delete timer;
+}
+
+void Circle::move(){
+    QList<QGraphicsItem *>colliding_items = collidingItems();
+        for(auto a: colliding_items){
+            if(typeid(*(a))== typeid(car)){
+                scene()->removeItem(this);
+                delete this;
+
+                return;
+
+            }
+        }
+     setPos(x(),y()+5);
+
+     if(y()>=scene()->height()){
+         scene()->removeItem(this);
+         delete this;
+     }
+
+}
+
